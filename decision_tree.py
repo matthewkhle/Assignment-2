c1d5b400e8a9e4ffca67aa8fa14a26e304060bc5
Matthew Le <mattmattmattle@gmail.com>
2021-02-25 22:46:18 -0800

    Adjusted program description

--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,9 @@
 # -------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Matthew Le
+# FILENAME: decision_tree.py
+# SPECIFICATION: This program takes three different sets of training data to train, and takes a test set, calculating the accuracy of each training set.
 # FOR: CS 4200- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 2 hours (approx)
 # -----------------------------------------------------------*/
 
 # IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
