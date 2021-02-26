691092b14ebe5cce605df8d0893f1d8a525bb855
Matthew Le <mattmattmattle@gmail.com>
2021-02-26 00:57:21 -0800

    knn.py completed
    Prints:
    
    Error Rate: 40.0%

--- knn.py
+++ knn.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Matthew Le
+# FILENAME: knn.py
+# SPECIFICATION: This program uses 1KNN to predict the value of each instance.
 # FOR: CS 4200- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 1 Hour (approx)
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
@@ -21,21 +21,39 @@ with open('binary_points.csv', 'r') as csvfile:
       if i > 0: #skipping the header
          db.append (row)
 
+incorrect = 0.0
+total = 0.0
 
 #loop your data to allow each instance to be your test set
 for i, instance in enumerate(db):
 
     #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]]
     #--> add your Python code here
-    # X =
+    X = []
+    for j, instance in enumerate(db):
+        Xtemp = []
+        if i != j:
+            Xtemp.append(instance[0])
+            Xtemp.append(instance[1])
+            X.append(Xtemp)
 
     #transform the original training classes to numbers and add to the vector Y removing the instance that will be used for testing in this iteration. For instance, Y = [1, 2, ,...]
     #--> add your Python code here
-    # Y =
+    Y = []
+    for j, instance in enumerate(db):
+        if i != j:
+            if instance[2] == '-':
+                Y.append(1)
+            elif instance[2] == '+':
+                Y.append(2)
 
     #store the test sample of this iteration in the vector testSample
     #--> add your Python code here
-    #testSample =
+    testSample = []
+    for j, instance in enumerate(db):
+            if i == j:
+                testSample.append(instance[0])
+                testSample.append(instance[1])
 
     #fitting the knn to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
@@ -44,13 +62,27 @@ for i, instance in enumerate(db):
     #use your test sample in this iteration to make the class prediction. For instance:
     #class_predicted = clf.predict([[1, 2]])[0]
     #--> add your Python code here
+    testClass = 0
+    for j, instance in enumerate(db):
+            if i == j:
+                if instance[2] == '-':
+                    testClass = 1
+                elif instance[2] == '+':
+                    testClass = 2
+
+    class_predicted = clf.predict([testSample])[0]
 
     #compare the prediction with the true label of the test instance to start calculating the error rate.
     #--> add your Python code here
 
+    if class_predicted != testClass:
+        incorrect += 1
+    total += 1
+
 #print the error rate
 #--> add your Python code here
-
+error_rate = (incorrect / total) * 100
+print('\nError Rate: ', error_rate, '%', sep='')
 
 
 
